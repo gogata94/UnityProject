1c3851b0fc063c4e1d009b3c0f66af7fd3431bbf
pufpafy <fdeliter@abv.bg>
2017-03-23 19:37:30 +0200

    Adding folder + Read potentiometer program

---
+++ Arduino Files/Read1potens.ino
@@ -0,0 +1,26 @@
+int data = 0;
+String consoleInputNumber;
+char set[10];
+ 
+void setup() {
+  Serial.begin(9600,SERIAL_8O1);  // start serial for output
+  pinMode(A1, INPUT);
+  pinMode(A0, INPUT);
+  
+  
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+   Serial.readBytes(set,1);
+   if (set[0] == 'A') {
+      data = analogRead(A0);   
+      Serial.println(data);
+      data = analogRead(A1);
+      Serial.println(data);  
+      delay(50);
+    }
+  } else { }
+  set[0]='0';
+}
+
